d7c21a17f24e988d194aae0d48028df7f06ecdf9
Eric Dain <eric.felker@gmail.com>
2022-11-11 05:45:25 -0500

    4b complete

---
+++ 2015/4b.rb
@@ -0,0 +1,18 @@
+require 'digest'
+
+key = 'ckczppom'
+
+i=0
+while true
+    i+=1
+    #puts "#{key}#{i}"
+    hash = Digest::MD5.hexdigest "#{key}#{i}"
+    #puts hash
+    if hash.start_with?('000000')
+        break
+    end
+end
+
+puts i
+
+#not 1605078 -- too high
